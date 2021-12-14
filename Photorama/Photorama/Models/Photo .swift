7c41e345ce9ca58c450ec6704d04bf636c3f46f5
arwabalawi <arwa62885@gmail.com>
2021-12-14 09:05:17 +0300

    completed

---
+++ Photorama/Photorama/Models/Photo .swift
@@ -0,0 +1,22 @@
+//
+//  Photo .swift
+//  Photorama
+//
+//  Created by arwa balawi on 08/05/1443 AH.
+//
+
+import Foundation
+
+class Photo :Codable {
+  let title: String
+  let remoteURL: URL?
+  let photoID: String
+  let dateTaken: Date
+  
+  enum CodingKeys: String, CodingKey {
+    case title
+    case remoteURL = "url_z"
+    case photoID = "id"
+    case dateTaken = "datetaken"
+  }
+}
