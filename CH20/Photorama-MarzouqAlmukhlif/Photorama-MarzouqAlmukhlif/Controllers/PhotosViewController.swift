185f9b23abdcdf18758932ad2a4863f631c90fb5
Marzouq Almukhlif <mhster_nice@Marzouqs-MacBook-Pro.local>
2021-12-14 09:18:35 +0300

    Finish CH20 and CH21

---
+++ CH20/Photorama-MarzouqAlmukhlif/Photorama-MarzouqAlmukhlif/Controllers/PhotosViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Photorama-MarzouqAlmukhlif
+//
+//  Created by Marzouq Almukhlif on 09/05/1443 AH.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+  
+  @IBOutlet private var imageView: UIImageView!
+  var store: PhotoStore!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    store.fetchInterestingPhotos {
+            (photosResult) in
+            switch photosResult {
+            case let .success(photos):
+                print("Successfully found \(photos.count) photos.")
+              if let firstPhoto = photos.first {
+                              self.updateImageView(for: firstPhoto)
+              }
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+    }
+  }
+  
+  
+  func updateImageView(for photo: Photo) {
+      store.fetchImage(for: photo) {
+          (imageResult) in
+          switch imageResult {
+          case let .success(image):
+              self.imageView.image = image
+          case let .failure(error):
+              print("Error downloading image: \(error)")
+          }
+  } }
+
+
+}
+
