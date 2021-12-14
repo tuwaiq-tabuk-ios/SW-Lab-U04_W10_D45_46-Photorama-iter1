c146d190356fa3172f0a21457d066765664e22f3
Maha S <mahas@Mahas-MacBook-Air.local>
2021-12-14 09:07:50 +0300

    Photorama iter1

---
+++ Photorama-MahaNasser-iter1/Photorama-MahaNasser/Models/Photo.swift
@@ -0,0 +1,23 @@
+//
+//  Photo.swift
+//  Photorama-MahaNasser
+//
+//  Created by Maha S on 12/12/2021.
+//
+
+import Foundation
+
+class Photo: Codable {
+  
+  let title: String
+  let remoteURL: URL?
+  let photoID: String
+  let dateTaken: Date
+  
+  enum CodingKeys: String, CodingKey {
+         case title
+         case remoteURL = "url_z"
+         case photoID = "id"
+         case dateTaken = "datetaken"
+ }
+}
