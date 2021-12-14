764e4be79bcf42def131c7f3e4806776427117de
عبدالعزيز البلوي <bdalzyzalblwy@MacBook-Air-bdalzyz.local>
2021-12-14 09:17:47 +0300

    Photorama

---
+++ Photorama-Abdulaziz-Saleem/Photorama-Abdulaziz-Saleem/model/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama-Abdulaziz-Saleem
+//
+//  Created by عبدالعزيز البلوي on 08/05/1443 AH.
+//
+
+import Foundation
+
+class Photo : Codable {
+  let title: String
+  let remoteURL: URL?
+  let photoID: String
+  let dateTaken: Date
+  
+  enum CodingKeys: String, CodingKey {
+    case title
+    case remoteURL = "url_z"
+    case photoID = "id"
+    case dateTaken = "datetaken"
+  }
+  
+}
+
