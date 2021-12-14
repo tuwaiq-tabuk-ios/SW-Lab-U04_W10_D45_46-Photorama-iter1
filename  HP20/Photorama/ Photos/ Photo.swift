9db2d06f67845656fc9184eae345aa3cad25c9d2
azooz alhwiti <azoozalhwiti@MacBook-Air-azooz.local>
2021-12-14 09:26:32 +0300

    Created and completed the HelloWorld project

---
+++  HP20/Photorama/ Photos/ Photo.swift
@@ -0,0 +1,20 @@
+//
+//   Photo.swift
+//  Photorama
+//
+//  Created by azooz alhwiti on 12/12/2021.
+//
+
+import Foundation
+class Photo: Codable {
+    let title: String
+  let remoteURL: URL?
+    let photoID: String
+    let dateTaken: Date
+  enum CodingKeys: String, CodingKey {
+          case title
+          case remoteURL = "url_z"
+          case photoID = "id"
+          case dateTaken = "datetaken"
+  }
+}
---
+++  HP20/Photorama/ Photos/PhotosViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by azooz alhwiti on 12/12/2021.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet private var imageView: UIImageView!
+  
+  var store: PhotoStore!
+  
+  
+  
+  
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+    store.fetchInterestingPhotos {
+           (photosResult) in
+           switch photosResult {
+           case let .success(photos):
+               print("Successfully found \(photos.count) photos.")
+            if let firstPhoto = photos.first {
+                            self.updateImageView(for: firstPhoto)
+                        }
+           case let .failure(error):
+            
+               print("Error fetching interesting photos: \(error)")
+           }
+   }
+  }
+  func updateImageView(for photo: Photo) {
+    store.fetchImage(for: photo) {
+        (imageResult) in
+
+          switch imageResult {
+          case let .success(image):
+          self.imageView.image = image
+          case let .failure(error):
+              print("Error downloading image: \(error)")
+          }
+  }
+    
+  }
+
+}
